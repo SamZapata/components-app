3ecfe664af09ea1edd19480e5887aae67bcc9476
samzapata <legionario.zapata@gmail.com>
2020-10-19 16:59:58 -0500

    add react container and presentation components

---
+++ src/shared/comments.js
@@ -0,0 +1,33 @@
+export const COMMENTS =
+  [
+    {
+      id: 0,
+      author: "John Lemon",
+      comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      date: "2012/10/17"
+    },
+    {
+      id: 1,
+      author: "Paul McVites",
+      comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      date: "2014/9/6"
+    },
+    {
+      id: 2,
+      author: "Michael Jaikishan",
+      comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      date: "2015/2/14"
+    },
+    {
+      id: 3,
+      author: "Ringo Starry",
+      comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      date: "2013/12/3"
+    },
+    {
+      id: 4,
+      author: "25 Cent",
+      comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      date: "2011/12/3"
+    },
+  ]
