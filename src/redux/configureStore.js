7a31cd8903accdfdb183ab499a36209599e262e7
samzapata <legionario.zapata@gmail.com>
2020-11-26 20:07:57 -0500

    update redux thunk

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -5,6 +5,8 @@ import { Promotions } from './promotions';
 import { Leaders } from './leaders';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger'
+import { createForms } from 'react-redux-form';
+import { InitialFeedback } from './forms';
 // import { Reducer, initialState } from './reducer'
 
 export const ConfigureStore = () => {
@@ -15,7 +17,10 @@ export const ConfigureStore = () => {
           dishes: Dishes,
           comments: Comments,
           promotions: Promotions,
-          leaders: Leaders
+          leaders: Leaders,
+          ...createForms({
+            feedback: InitialFeedback
+          })
         }),
         applyMiddleware(thunk, logger)
     );
