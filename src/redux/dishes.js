2e2df3760292b079594ffc4bed855cd4b11af780
samzapata <legionario.zapata@gmail.com>
2020-12-29 19:18:18 -0500

    add task i part i, the component status is loading

--- src/redux/dishes.js
+++ src/redux/dishes.js
@@ -12,8 +12,8 @@ export const Dishes = (state = {
     case ActionTypes.DISHES_LOADING:
       return {...state, isLoading: true, errMess: null, dishes: []};
 
-    case ActionTypes.ADD_DISHES:
-      return {...state, isLoading: false, errMess: action.payload, dishes: []};
+    case ActionTypes.DISHES_FAILED:
+      return {...state, isLoading: false, errMess: action.payload};
 
     default:
       return state;
